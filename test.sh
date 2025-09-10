7e31550d3ba1365726c9ad3182e27c9b8945f114
smolen92 <edo234@gmail.com>
2025-09-10 10:23:12 +0200

    load config from conf file, not yet tested

---
+++ test.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+# todo log mv and btrfs output?
+# todo test - pid - rsnapshot can see pidfile created by this script, backup - all level, no level
+
+# bug pid file is not deleted after the script is finished
+# bug backup rotation is not hit
+
+#default config location
+#todo change to /etc/rsnapshot.conf
+config_file="rsnapshot.conf"
+
+declare -a commands=()
+
+#parse arguments
+while [ "$1" != "" ]
+do
+	case $1 in
+		-c )	shift
+			config_file=$1
+			;;
+
+		* )	commands+=($1)
+			;;
+	esac
+
+	shift
+done
+
+if [ "${#commands[@]}" -gt 1 ]
+then
+	echo "multiple command entered, enter only one command"
+fi
+
+if ! [ -f $config_file ]
+then
+	echo "config file not found"
+	exit 1
+fi
+
+declare -a backups_level=()
+declare -a backups_level_count=()
+
+#read config file
+while read -r line
+do
+	first_char=${line:0:1}
+	#skip if first char is comment or empty line
+	if [[ $first_char != "#" ]] && [[ -n $first_char ]]
+	then
+
+		declare -a elements=()
+		#break line into elements
+		for word in $line
+		do
+			elements+=($word)
+		done
+
+		#save variables
+		if [[ ${elements[0]} == "snapshot_root" ]]
+		then
+			snapshot_root="${elements[1]}"
+		fi
+
+		if [[ ${elements[0]} == "retain" ]] 
+		then
+			backups_level+=(${elements[1]})	
+			backups_level_count+=(${elements[2]})	
+		fi
+
+		if [[ ${elements[0]} == "logfile" ]] 
+		then
+			logfile="${elements[1]}"
+		fi
+
+		if [[ ${elements[0]} == "lockfile" ]] 
+		then
+			lockfile="${elements[1]}"
+		fi
+
+		if [[ ${elements[0]} == "sync_first" ]]
+		then
+			sync_first="${elements[1]}"
+		fi
+
+	fi
+done < $config_file
+
+#check pid file
+if [ -f $lockfile ]
+then
+	pgrep -F $lockfile
+	if [ $? -eq 1 ]
+	then
+		rm $lockfile
+	else 
+		echo "rsnapshot instance is already running" >> $logfile
+		exit
+	fi
+fi
+
+echo $$ > $lockfile
+
+#check sync first 
+if [[ $sync_first != "1" ]]
+then
+	echo "sync not turn on" >> $logfile
+fi
+
+#check if command specified is found in config file and save its index
+declare -i command_found=0
+
+for i in "${!backups_level[@]}"
+do
+	if [[ $commands == ${backups_level[$i]} ]]
+	then
+		command_found=1
+		command_index=$i
+		break
+	fi
+done
+
+if [ $command_found -ne 1 ]
+then
+	echo "command not found" >> $logfile
+	exit 1
+fi
+
+echo "$commands: started" >> $logfile
+
+#delete oldest snapshot if it exist
+if [ -d "${backups_level[$command_index]}.$((${backups_level_count[$command_index]}-1))" ]
+then
+	echo "btrfs subvolume delete \"${backups_level[$command_index]}.$((${backups_level_count[$command_index]}-1))\" "
+	btrfs subvolume delete "${backups_level[$command_index]}.$((${backups_level_count[$command_index]}-1))"
+	btrfs_return_value=$?
+	if [ $btrfs_return_value -ne 0 ]
+	then
+		echo "Error: Failed to delete snapshot ${backups_level[$command_index]}.$((${backups_level_count[$command_index]}-1)). Error code: $btrfs_return_value" >> $logfile
+		exit 1
+	fi
+fi
+
+#rotate backups
+for ((j=${backups_level_count[$command_index]}-1 ; j>=1; j--))
+do
+	if [ -d ${backups_level[$command_index]}.$(($j-1)) ]
+	then
+		echo "mv \"${backups_level[$command_index]}.$(($j-1)) ${backups_level[$command_index]}.$j\""
+		mv "${backups_level[$command_index]}.$(($j-1)) ${backups_level[$command_index]}.$j"
+		if [ -d ${backups_level[$command_index]}.$(($j-1)) ]
+		then
+			echo "Error: failed to move ${backups_level[$command_index]}.$(($j-1))" >> $logfile
+			exit 1
+		fi
+	fi
+done
+
+#if first interval 
+if [ $command_index -eq 0 ]
+then
+	if [ -d $snapshot_root/.sync ]
+	then
+		echo "btrfs subvolume snapshot $snapshot_root/.sync $snapshot_root/${backups_level[$command_index]}.0"
+		btrfs subvolume create "$snapshot_root/.sync $snapshot_root/${backups_level[$command_index]}.0"
+		btrfs_return_value=$?
+		if [ $btrfs_return_value -ne 0 ]
+		then
+			echo "Error: Failed to create snapshot $snapshot_root/${backups_level[$command_index]}.0 Error code: $btrfs_return_value" >> $logfile
+			exit 1
+		fi
+	else
+		echo ".sync doesn't exist" >> $logfile
+	fi
+else
+	if [ -d ${backups_level[$(($command_index-1))]}.${backups_level_count[$(($command_index-1))]} ]
+	then
+		echo "mv \"${backups_level[$(($command_index-1))]}.${backups_level_count[$(($command_index-1))]} ${backups_level[$command_index]}.0\" "
+		mv "${backups_level[$(($command_index-1))]}.${backups_level_count[$(($command_index-1))]} ${backups_level[$command_index]}.0"
+		if [ -d ${backups_level[$(($command_index-1))]}.${backups_level_count[$(($command_index-1))]} ] 
+		then
+			echo "Error: failed to move ${backups_level[$(($command_index-1))]}.${backups_level_count[$(($command_index-1))]}" >> $logfile 
+			exit 1
+		fi
+	else
+		echo "${backups_level[$(($command_index-1))]}.${backups_level_count[$(($command_index-1))]} doesn't exist yet" >> $logfile
+	fi
+fi
+
+echo "$commands: completed successfully"
+
